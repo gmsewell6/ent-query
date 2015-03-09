f6ac6b29822f766e89c6d1e49304b40d19ded181
Brad Leupen <brad@entrinsik.com>
2015-03-09 11:23:19 -0400

    added gulp added test with coverage

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+var cover = require('gulp-coverage');
+var istanbul = require('gulp-istanbul');
+
+gulp.task('test', function (done) {
+    gulp.src(['lib/**/*'])
+        .pipe(istanbul())
+        .pipe(istanbul.hookRequire())
+        .on('finish', function () {
+            gulp.src(['test/*.js'])
+                .pipe(mocha())
+                .pipe(istanbul.writeReports())
+                .on('end', done)
+        });
+});
