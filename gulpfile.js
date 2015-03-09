9c9530b131c4d46d5344690e6f26ee6452c7c7f8
Brad Leupen <brad@entrinsik.com>
2015-03-09 11:31:48 -0400

    added version bump, release tagging

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var istanbul = require('gulp-istanbul');
+var bump = require('gulp-bump');
+var git = require('gulp-git');
+var filter = require('gulp-filter');
+var tag = require('gulp-tag-version');
 
 gulp.task('test', function (done) {
     gulp.src(['lib/**/*'])
@@ -14,4 +18,35 @@ gulp.task('test', function (done) {
                 .pipe(istanbul.writeReports())
                 .on('end', done)
         });
+});
+
+function inc(importance) {
+    // get all the files to bump version in
+    return gulp.src(['./package.json', './bower.json'])
+        // bump the version number in those files
+        .pipe(bump({type: importance}))
+
+        // save it back to filesystem
+        .pipe(gulp.dest('./'))
+
+        // commit the changed version number
+        .pipe(git.commit('bumps package version'))
+
+        // read only one file to get the version number
+        .pipe(filter('package.json'))
+
+        // **tag it in the repository**
+        .pipe(tag());
+}
+
+gulp.task('patch', function () {
+    return inc('patch');
+});
+
+gulp.task('feature', function () {
+    return inc('minor');
+});
+
+gulp.task('release', function () {
+    return inc('major');
 });
